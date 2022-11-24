2dbcfe03dcfee45146546985314b7c5d6dd8260b
spandans20 <118908939+spandans20@users.noreply.github.com>
2022-11-24 14:56:35 +0530

    Update index.js
    Updating the output and port details

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! This is your host Spandan trying to rule over Jenkins, Github and Docker');
 });
 
-var server = app.listen(3002, function () {
+var server = app.listen(3003, function () {
   var host = server.address().address;
   var port = server.address().port;
 
